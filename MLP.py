efb7b9e05f99a9fc4af5b9a7cd6f90a47a42ba13
skowronskid <skdamianda@gmail.com>
2023-02-25 15:46:38 +0100

    docstrings

--- MLP.py
+++ MLP.py
@@ -16,6 +16,33 @@ def linear(x):
 
 
 class Layer():      
+    """_summary_
+    A layer class for a multi-layer perceptron
+    
+    _parameters_
+    nr_neurons: int
+        The number of neurons in the layer. 
+    weights: np.array
+        The weights of the layer. The shape of the array should be (nr_neurons, nr_inputs). Each row represents the weights of one neuron.
+    biases: np.array
+        The biases of the layer. The shape of the array should be (nr_neurons, 1). Each row represents the bias of one neuron.
+    func: Callable
+        The activation function of the layer. The function should take a numpy array as input and return a numpy array as output.
+    
+    _methods_
+    forward(X)
+        Calculates the output of the layer for the given input X and returns it. 
+    summary()
+        Returns a dictionary containing the weights, biases and function of the layer. 
+    set_weights(weights)
+        Sets the weights of the layer.
+    set_biases(biases)
+        Sets the biases of the layer
+    set_function(func)
+        Sets the function of the layer
+    set_nr_neurons(nr_neurons)
+        Sets the number of neurons of the layer
+    """
     
     
     def __init__(self, nr_neurons, weights:np.array=None, biases:np.array=None, func:Callable=None):
@@ -29,7 +56,7 @@ class Layer():
         self.function = func
         
         
-    def forward(self, X):
+    def forward(self, X:np.array):
         if any(el is None for el in [self.weights, self.biases, self.function]):
             raise AttributeError("Something in this layer is missing")
         if len(X.shape) == 1:
@@ -48,23 +75,23 @@ class Layer():
         return dict
     
     
-    def set_weights(self, weights):
+    def set_weights(self, weights:np.array):
         if weights.shape[0] != self.nr_neurons:
             raise Warning("The number of neurons on this layer seems to be different to the number of weights given. Maybe use reshape to state the shape explicitly")
         self.weights = weights
         
         
-    def set_biases(self, biases):
+    def set_biases(self, biases:np.array):
         # if len(biases) != self.nr_neurons:
             # raise Warning("The number of neurons on this layer seems to be different to the number of biases given.")
         self.biases = biases
         
     
-    def set_function(self,func):
+    def set_function(self,func:Callable):
         self.function = func
         
     
-    def set_nr_neurons(self,nr_neurons):
+    def set_nr_neurons(self,nr_neurons:int):
         self.nr_neurons = nr_neurons
         
      
@@ -73,6 +100,25 @@ class Layer():
 
 
 class MLP():           
+    """_summary_
+    A multi-layer perceptron class
+    
+    _parameters_
+    layers: dict
+        A dictionary containing the layers of the MLP. The keys are the names of the layers and the values are the layers themselves.
+    weights: list
+        A list containing the weights of the MLP. The weights are stored in the same order as the layers in the layers dictionary.
+    biases: list
+        A list containing the biases of the MLP. The biases are stored in the same order as the layers in the layers dictionary.
+    
+    _methods_
+    add(layer:Layer, name:str=None)
+        Adds a layer to the MLP. The name of the layer is optional. If no name is given, the name of the layer will be 'Layer' + the number of layers already in the MLP.
+    predict(inputs)
+        Predicts the output for the given input.
+    summary()
+        Returns a dictionary containing the weights, biases and functions of all layers in the MLP.
+    """
     
     
     def __init__(self):
@@ -103,11 +149,22 @@ class MLP():
         for name,layer in zip(self.layers.keys(), self.layers.values()):
             summary_dict[name] = layer.summary()
         return summary_dict
+    
+
+    def pop(self):
+        if len(self.layers) == 0:
+            raise Warning("There are no layers in this MLP")
+        self.layers.popitem()
+        self.weights.pop()
+        self.biases.pop()
+        
+        
               
         
         
         
 def plot_predictions(mlp:MLP, df_train:pd.DataFrame, df_test:pd.DataFrame):
+    #Plots the predictions of the MLP on the training and test data side by side, as well as the MSE of the predictions   
     warnings.filterwarnings('ignore')
 
     y_train = mlp.predict(df_train[["x"]])
@@ -129,6 +186,7 @@ def plot_predictions(mlp:MLP, df_train:pd.DataFrame, df_test:pd.DataFrame):
     
     
 def plot_data(title:str, df_train:pd.DataFrame, df_test:pd.DataFrame):
+    # Plots the training and test data side by side
     fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(10,4))
 
     ax[0].scatter(x=df_train["x"], y = df_train['y'])
